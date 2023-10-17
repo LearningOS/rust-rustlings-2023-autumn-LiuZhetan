8b36c62dbfe7e506c4e38ba517ad11df1fe34a1b
LiuZhetan <liuzhetande@foxmail.com>
2023-10-17 19:33:54 +0800

    fix clippy1

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -12,10 +12,9 @@
 // I AM DONE
 
 use std::f32;
-use f32::consts::PI;
 
 fn main() {
-    let pi = PI;
+    let pi = f32::consts::PI;
     let radius = 5.00f32;
 
     let area = pi * f32::powi(radius, 2);
